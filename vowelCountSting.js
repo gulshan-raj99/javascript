aa766ebd15a68fbec01910a1ade23e454302fb05
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-11-23 12:05:36 +0530

    Count vowels in a string

---
+++ vowelCountSting.js
@@ -0,0 +1,12 @@
+function countVowels(str) {
+    let vowels = "aeiouAEIOU";
+    let count = 0;
+
+    for (let char of str) {
+        if (vowels.includes(char))
+            count++;
+    }
+    return count;
+}
+
+console.log(countVowels('Gulshan Raj'));
