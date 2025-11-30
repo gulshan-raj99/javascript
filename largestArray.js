f5120bda51a900aa0ef787236bc4d5d1cba1e4e1
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-11-30 08:04:31 +0530

    second largest number of  element array

---
+++ largestArray.js
@@ -0,0 +1,36 @@
+const SecondLargest = (arr) => {
+    if(arr.length < 2) return null;
+
+  let largest = arr[0];
+  let second = arr[1];
+  
+  if(second > largest) {
+      let temp = largest;
+      largest = second;
+      second = temp;
+  }
+  for (let i = 2; i< arr.length; i++) {
+      let num = arr[i];
+      
+      if(num > largest) {
+          second = largest;
+          largest = num;
+      } else if (num > second && num !== largest) {
+          second = num;
+      }
+  }
+  return second;
+}
+
+console.log(SecondLargest([2,3,45,67,44,57]));
+
+function secondElement(arr){
+
+    let unique = [...new Set(arr)];
+
+    // sort descending
+    unique.sort((a, b) => b -a);
+    
+    return unique[1];
+}
+console.log(secondElement([10,20,45,60,26]));
