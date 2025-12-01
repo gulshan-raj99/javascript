7e316852bfaab4acecd393f168be2e638f3eaa75
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-01 08:51:31 +0530

    prime number and range of primenumber

---
+++ primeNumber.js
@@ -0,0 +1,49 @@
+const isPrime = (num) => {
+    for(let i = 2; i<num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrime(7));
+
+
+// prime number in given number limit
+const printPrime = (limit) => {
+
+    for(let num = 1; num<= limit; num ++){
+
+        let isPrime = true;
+        
+        for(let i = 2; i<num; i++) {
+            if(num % i === 0) {
+                isPrime = false;
+                break;
+            };
+        }
+        if(isPrime){
+            console.log(num);
+        };
+
+    }  
+}
+printPrime(50);
+
+
+const rangePrime = (a,b) => {
+    for(let i = a; i <=b ; i++){
+        let isPrime = true;
+        
+        for(let j= 2; j< i; j++){
+            if(i % j === 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            console.log(i);
+        }
+    }
+}
+rangePrime(20,56);
