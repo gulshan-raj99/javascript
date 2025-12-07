fe75fd68486b622b3b470ecb0945e37d00de4b98
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-07 06:46:04 +0530

    array sort with function

--- sortArray.js
+++ sortArray.js
@@ -11,3 +11,17 @@ for(let i = 0; i< arr.length; i++){
 }
 console.log(arr);
 console.log(arr[1]);
+
+function sorting(arr) {
+    for(let i = 0; i<arr.length;i++){
+        for(let j = 0; j< arr.length-1;j++){
+            if(arr[j] < arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(sorting([23,45,66,77,11]));
