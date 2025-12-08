87cae707b82d334559cfcb703c8dd9e5c06395b5
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-08 06:16:48 +0530

    count vowel in a string

---
+++ vowel.js
@@ -0,0 +1,20 @@
+//aab
+function vowelCount(str){
+    let count = 0;
+
+    for(let i = 0; i<str.length;i++){
+        let ch = str[i].toLowerCase();
+
+        if(ch === 'a' ||
+             ch === 'e' ||
+              ch === 'i' ||
+              ch === 'o' ||
+              ch === 'u') {
+            count ++;
+        }
+        
+    }
+    return count;
+}
+console.log(vowelCount('Apple'));
+console.log(vowelCount("Gulshan Raj"));
