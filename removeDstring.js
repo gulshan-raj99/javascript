49c2930579490a2e2483c5bd79c02f28ca46d243
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-08 06:37:00 +0530

    remove duplicate string without using includes method

--- removeDstring.js
+++ removeDstring.js
@@ -20,3 +20,23 @@ const duplicate = (str) => {
     return result;
 }
 console.log(duplicate("Bananana"))
+
+// type 3
+function duplicate(str){
+    let result = "";
+    for(let i=0; i<str.length;i++){
+        let ch = str[i];
+        let found = false;
+        for(let j = 0; j<result.length;j++){
+            if (result[j] === ch){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            result += ch;
+        }
+    }
+    return result;
+}
+console.log(duplicate("Banana"));
