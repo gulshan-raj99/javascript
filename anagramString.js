75e64e86a3eb0607dbffeeb2fcefc341e134d903
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-12 07:33:06 +0530

    anagram string the loop

--- anagramString.js
+++ anagramString.js
@@ -6,3 +6,36 @@ function isAnagram(str1, str2) {
 }
 
 console.log(isAnagram('listen', 'silent'));
+
+function anagram(str1, str2){
+    if(str1.length === str2.length){
+        return true;
+    }
+    let freq1 = {};
+    let freq2 = {};
+    
+    for(let i =0; i<str1.length;i++){
+        let ch = str[i];
+        if(freq1[ch]){
+            freq1[ch]++;
+        } else {
+            freq1[ch]=1;
+        }
+    }
+    
+    for(let j =0; j<str2.lenth;j++){
+        let ch = str[j];
+        if(freq2[ch]){
+            freq2[ch]++;
+        } else {
+            freq2 = 1;
+        }
+    }
+    for(let key in freq1){
+        if(freq1[key] !== freq2[key]){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(anagram("silent", "listen"));
