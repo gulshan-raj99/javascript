2bd5664316c98cda6894548168093d3f0d67c26f
gulshan-raj99 <gulshanraj1706@gmail.com>
2025-12-16 07:41:03 +0530

    sum of digit in a number

--- sumArray.js
+++ sumArray.js
@@ -6,3 +6,16 @@ function sumOfArray(arr){
     return sum;
 }
 console.log(sumOfArray([12,23,45]));
+
+// sum of digit
+function sumOfDigit(num){
+    let sum = 0;
+    
+    while(num>0) {
+        let digit = num % 10;
+        sum = sum +digit;
+        num = (num-digit)/10;
+    }
+    return sum;
+}
+console.log(sumOfDigit(125));
